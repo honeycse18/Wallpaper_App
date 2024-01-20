9a94e779385ab9123497d37244de76b0e1ded3b0
honey <honey.nubtk.cse@gmail.com>
2024-01-21 02:00:35 +0600

    Home screen

--- lib/Detail_Screen.dart
+++ lib/Detail_Screen.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class Details extends StatelessWidget {
+  const Details(data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wallpaper_app/Detail_Screen.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  final Stream<QuerySnapshot> _imageStream =
+      FirebaseFirestore.instance.collection('images').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _imageStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.65,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Hero(
+                        tag: data['img'],
+                        child: Material(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  data['img'],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(10),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (_) => Details(data['img']))),
+                            ),
+                          ),
+                        ));
+                  }).toList(),
+                );
+              },
+            )),
+      ),
     );
   }
 }
